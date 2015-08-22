451ae26594efbc3ef5cd38b6edb27512b3eb1ddf
Fernando <framirez@imaginabit.com>
2015-08-22 17:50:44 +0100

    aa

--- sendscore.js
+++ sendscore.js
@@ -1,14 +1,5 @@
-$.getScript("http://www.parsecdn.com/js/parse-1.5.0.min.js", function(){
-   console.log("Script loaded but not necessarily executed.");
-   Parse.initialize("Vz8t62Nh4003qMWIfJL7ORRRwWXtMVJtWBbOrxJ5", "ePqAgI4cUuC5NNtGghJ68AHLYkh9jV0311MYoL1I");
-
-
-
-   sendscore('Fernando',9999999,213232,2121);
-   sendscore('Roberto',999999,21332,121);
-   sendscore('Jonay',9999999,213232,2121);
-
-});
+// need http://www.parsecdn.com/js/parse-1.5.0.min.js"
+// and parse_key.js Parse.initialize with the keys
 
 var sendscore = function(name,score,icecream,destroyed) {
     var GameScore = Parse.Object.extend("GameScore");
